941f92b5e87f6998191447ee3051f363cc9cfb30
sotraore3 <sotraore3@turing.local.isima.fr>
2022-06-21 12:02:50 +0200

    Implementation

--- SoTraoreIndiv/serpent.c
+++ SoTraoreIndiv/serpent.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <SDL2/SDL.h>
-#include<stdlib.h>
+#include "serpent.h"
 
 void SDL_ErrorCase(char * msg) {
 	SDL_Log("%s %s\n", msg, SDL_GetError()) ;
@@ -18,24 +16,18 @@ void SDL_DrawRect(SDL_Renderer * renderer, SDL_Rect rect, int r, int g, int b, S
  
 	if (SDL_RenderDrawRect(renderer, &rect) != 0)
 		SDL_ErrorCase("Trouble with draw rect") ;
-
-	SDL_RendererPresent(renderer) ;
 }
 
 void SDL_DrawLine(SDL_Renderer * renderer, int x1, int y1, int x2, int y2) 
 {
 	if (SDL_RenderDrawLine(renderer, x1, y1, x2, y2) != 0) 
 		SDL_ErrorCase("Trouble with draw line") ;
-
-	SDL_RendererPresent(renderer) ;
 }
 
 void SDL_DrawPoint(SDL_Renderer * renderer, int x, int y) 
 {
 	if (SDL_RenderDrawPoint(renderer, x, y) != 0) 
 		SDL_ErrorCase("Trouble with draw point") ;
-
-	SDL_RenderPresent(renderer) ;
 }
 
 /*
@@ -45,7 +37,7 @@ void SDL_DrawPoint(SDL_Renderer * renderer, int x, int y)
 
 void SDL_DrawSnake(SDL_Renderer * renderer, int x, int y) 
 {
-
+	
 
 }
 
