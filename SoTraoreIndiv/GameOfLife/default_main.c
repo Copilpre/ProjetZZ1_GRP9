017b1edceb03410c7a78c7ffd7fadb13d431d8d6
sotraore3 <sotraore3@turing.local.isima.fr>
2022-06-22 23:57:14 +0200

    GameOfLife ne marche pas car il y'a un de referencement de la Bu SDL2 	Peut être qu'il faudrait l'installer

--- SoTraoreIndiv/GameOfLife/default_main.c
+++ SoTraoreIndiv/GameOfLife/default_main.c
@@ -1,12 +1,25 @@
-#include "dessin.h"
+#include "compte_voisin.h"
+#include "grille.h"
 #define H 800
 #define W 800
 #define TAILLE 20
+#include <SDL2/SDL.h>
 
-int main(int argc, char ** argv) {
+
+void SDL_ErrorCase(char * msg) ;
+void SDL_DrawRect(SDL_Renderer * renderer, SDL_Rect rect, int r, int g, int b, SDL_bool fill) ; 
+void SDL_DrawWindowOfGame(SDL_Renderer * renderer, int nbElts, int h, int w) ;
+void SDL_ChangeColor(SDL_Renderer * renderer, SDL_Rect rect, SDL_Color c) ;
+
+
+int main() {
 	SDL_Window * window ;
 	SDL_Renderer * renderer ;
-	
+	SDL_bool keepLoop = SDL_TRUE ;
+	SDL_Event event ;
+	SDL_Rect rect ; 
+	SDL_Color c = {0, 0, 0, 0} ;
+
 	window = SDL_CreateWindow("GameOfLife",
 			SDL_WINDOWPOS_CENTERED,
 			SDL_WINDOWPOS_CENTERED,
@@ -21,50 +34,56 @@ int main(int argc, char ** argv) {
 	if (renderer == 0) 
 		SDL_ErrorCase("Trouble with renderer") ;
 
-	int w, h, i = 0, j = 0, vitesse = 0, taille ;
+	int w, h, i = 0, j = 0, vitesse = 0, taille, code = 0 ;
 
 	SDL_GetWindowSize(window, &w, &h) ;
 
-	SDL_bool keepLoop = SDL_TRUE , newColor = SDL_FALSE ;
 
-	SDL_Event event ;
 	taille = TAILLE ;
-	SDL_Rect rect ; 
+	
+	// Variables globales
+	Tvoisin = initGrille(TAILLE) ;
+	grille = initGrille(TAILLE) ;
+
 	rect.w = (int)(w/taille) ;
 	rect.h = (int)(h/taille) ;			
 
 	SDL_DrawWindowOfGame(renderer, taille, h, w) ;
 	
-	while (keepLoop) {
+	while (keepLoop || SDL_PollEvent(&event)) {
 
 		while (SDL_PollEvent(&event)) 
 		{
-			switch (event.type) 
-			{
+			switch (event.type) {
 				case SDL_BUTTON_LEFT :
 					vitesse = (vitesse < 100) ? vitesse+10 : 100 ;
 					break ;
 				case SDL_BUTTON_RIGHT :
 					vitesse =  (vitesse > 10) ? vitesse-10 : 10;
 					break ;
-				case SDL_MOUSEBUTTONDOWN :
+				case SDL_MOUSEBUTTONDOWN :   
 					SDL_GetMouseState(&i, &j) ;
 					i = (int)(i / rect.w) ;
 					j = (int)(j / rect.h) ;
 					rect.x = i * rect.w ;
 					rect.y = j * rect.h ;
-					// Ici on doit prendre en compte certains 
-					// changement au niveau de l'ecran et de la grille 
-					newColor = SDL_TRUE ;
-					SDL_ChangeColor(renderer, rect, newColor) ;
-					break ;
+
+      					if (SDL_GetMouseState(NULL, NULL) & 
+          						SDL_BUTTON(SDL_BUTTON_LEFT) ) {
+						c.r = c.g = c.b = 255 ;
+						code = 1 ;
+						SDL_ChangeColor(renderer, rect, c) ;
+      					} else if (SDL_GetMouseState(NULL, NULL) & 
+                 					SDL_BUTTON(SDL_BUTTON_RIGHT) ) { 
+						c.r = c.g = c.b = 0 ;
+						code = 0 ;
+						SDL_ChangeColor(renderer, rect, c) ;
+      					}
+      					break;
 				case SDL_WINDOWEVENT_CLOSE :
 					keepLoop = SDL_FALSE ;
 					break ;
 				case SDL_QUIT :
-					keepLoop = SDL_FALSE ;
-					break ;
-				case SDL_WINDOWEVENT_LEAVE :
 					keepLoop = SDL_FALSE ;
 					break ;
 				default :
@@ -81,4 +100,48 @@ int main(int argc, char ** argv) {
 	SDL_Quit() ;
 
 	exit(EXIT_FAILURE) ;
+}
+
+void SDL_ErrorCase(char * msg) {
+	SDL_Log("%s %s\n", msg, SDL_GetError()) ;
+	exit(EXIT_FAILURE) ;
+}
+
+void SDL_DrawRect(SDL_Renderer * renderer, SDL_Rect rect, int r, int g, int b, SDL_bool fill) 
+{
+	if (SDL_SetRenderDrawColor(renderer, r, g, b, SDL_ALPHA_OPAQUE) != 0)
+		SDL_ErrorCase("Trouble with draw color") ;
+
+	if (fill == SDL_TRUE)
+	if (SDL_RenderFillRect(renderer, &rect) != 0)
+		SDL_ErrorCase("Trouble with fill rect") ;
+ 
+	if (SDL_RenderDrawRect(renderer, &rect) != 0)
+		SDL_ErrorCase("Trouble with draw rect") ;
+}
+
+void SDL_DrawWindowOfGame(SDL_Renderer * renderer, int nbElts, int h, int w) 
+{
+	// Mise de la couleur de fond à noir 
+	if (SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255) != 0)
+		SDL_ErrorCase("Trouble with renderer") ;
+	
+	int pasH = h/nbElts, pasW = w/nbElts ;
+
+	for (int i=0; i<nbElts; i++) 	
+	{
+		if (SDL_RenderDrawLine(renderer, pasH*i, 0, pasH*i, w) != 0)
+			SDL_ErrorCase("Trouble with draw line") ;
+
+		if (SDL_RenderDrawLine(renderer, 0, pasW*i, h, pasW*i) != 0)
+			SDL_ErrorCase("Trouble with draw line") ;
+	}
+}
+
+// rect = {i*pasH, j*pasW, pasH, pasW} ;
+
+void SDL_ChangeColor(SDL_Renderer * renderer, SDL_Rect rect, SDL_Color c) 
+{
+	// Mise à rouge de la case ciblée
+	SDL_DrawRect(renderer, rect, c.r, c.g, c.b, SDL_TRUE) ; 
 }
