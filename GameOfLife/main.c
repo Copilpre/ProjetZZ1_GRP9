4a3c73c607be649a58528055b14f92141ba25ee8
copilpre <copilpre@turing.local.isima.fr>
2022-06-22 11:55:18 +0200

    modification du main

--- GameOfLife/main.c
+++ GameOfLife/main.c
@@ -7,13 +7,14 @@
 
 int main()
 {
-    //faire toute l'initialisation
+     //faire toute l'initialisation
 
 
 
     
     
-    menu(masqueNaissance,masqueSurvie);
+    char mode = menu(masqueNaissance,masqueSurvie);
+
 
 	char type;
 	int i,j;
@@ -64,11 +65,13 @@ int main()
 					break ;
 			}
             int compteurDifferences=0;
+            int nbrColonnes =20;
+            int nbrLignes=20;
             for(int i;i<nbrLignes;i++)
                 {
                     for(int j;j < nbrColonnes; j++)
                         {
-                        if (mode==torrique) //mettre la bonne variable
+                        if (mode=='i') //mettre la bonne variable
                             {
                             if (Tableau1[i][j]==1)        
                                 {Tableau2[i][j]= masqueSurvie[nb_voisins_torrique(**tab , i, j, nrbLignes, int nbrColonnes)];}
@@ -89,7 +92,10 @@ int main()
 
             if (compteurDifferences == 0)
                 {printf("Etat stable atteind");} 
-			SDL_RenderPresent(renderer) ;
+			
+            //il faut rentrer les bonnes variables
+            affichageTableau(int ligne, int col,int tab[ligne][col],SDL_Renderer * renderer,SDL_Window * window);
+            SDL_RenderPresent(renderer) ;
 			SDL_Delay(vitesse) ;
 		}
 	}
