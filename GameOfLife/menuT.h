c44c243b3e85757d0bed3db16de7e9cc1409f658
copilpre <copilpre@turing.local.isima.fr>
2022-06-22 11:22:07 +0200

    Ajout des .h

---
+++ GameOfLife/menuT.h
@@ -0,0 +1,10 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <SDL2/SDL_ttf.h>
+#include <string.h>
+
+
+int menu(SDL_bool * masqueNaissance, SDL_bool * masqueSurvie);
