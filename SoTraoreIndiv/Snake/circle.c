4e5f07858410d00e63f844535047a05f79841872
sotraore3 <sotraore3@ada.local.isima.fr>
2022-06-23 15:23:20 +0200

    Juste d'une d'erreur au niveau de drawSnake

--- SoTraoreIndiv/Snake/circle.c
+++ SoTraoreIndiv/Snake/circle.c
@@ -1,17 +1,4 @@
 #include "circle.h"
-#include <math.h>
-#define TAILLE 40
-#include "serpent.h"
-
-typedef struct circle {
-	int rayon ;	
-	float angle ;
-} circle_t ;
-
-typedef struct point {
-	int x ;
-	int y ;
-} point_t ;
 
 circle_t * polaire(circle_t * c1, float angle) {
 	circle_t * c = malloc(sizeof(circle_t)) ;
--- SoTraoreIndiv/Snake/default_main.c
+++ SoTraoreIndiv/Snake/default_main.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include "serpent.h"
 
-int main(int argc, char ** argc) {
+int main(int argc, char ** argc) 
+{
 	SDL_Window * window ;
 	SDL_Renderer * renderer ;
+	window = SDL_CreateWindow("Snake",
+			SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED,
+			800, 800, 0
+		) ;
+			
+	if (window == NULL) 
+		SDL_ErrorCase("Trouble with window") ;
 
+	renderer = SDL_CreateRender(window, -1, 0) ;
 
+	if (renderer == 0) 
+		SDL_Errorcase("Trouble with renderer") ;
+
+	point_t * origin = NULL ;
+
+	origin = malloc(sizeof(point_t)) ;
+	origin->x = 150 ;
+	origin->y = 150 ;
+	
+	SDL_DrawSnake(renderer, origin) ;
 
 	SDL_DestroyRenderer(renderer) ;
 	SDL_DestroyWindow(window) ;
