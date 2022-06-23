f56356ddd462416f5392e517d3b02da6538e6c03
PietroLabazuini <pierrotlabazuy@gmail.com>
2022-06-23 14:31:35 +0200

    jeu fonctionnel

---
+++ JeuMarkov/sceneSDL.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+
+void initSDL(int currentRoom,int currMood,float barreM,float barreD,float barreJ,SDL_Renderer * renderer,int windowW,int windowH);
