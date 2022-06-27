2124bc89cb6dd78a991aaf18e65709d3c2e7ca8c
SoTraore <107843654+SoTraore@users.noreply.github.com>
2022-06-27 14:38:27 +0200

    Update pile.c

--- ProjetApprentissage/pile.c
+++ ProjetApprentissage/pile.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAILLE 4
 
+typedef struct lineTab {
+	int T[TAILLE] ;
+} lineTab_t ;
 
 typedef struct pile
 {
     int nbElts;
     int position;
-    int * tab; 
+    lineTab_t * tab ; 
 }pile_t;
 
 pile_t * init_pile(int taille)
 {
     pile_t * nouv = NULL;
     nouv = malloc(sizeof(pile_t));
-    nouv -> nbElts = taille;
-    nouv -> tab = malloc(sizeof(int) * taille);
-    nouv -> position = -1;
+    nouv->nbElts = taille;
+    nouv->tab = malloc(sizeof(lineTab_t) * taille);
+    nouv->position = -1;
     return nouv;
 }
 
@@ -26,7 +30,7 @@ int EstVidePile(pile_t *p)
 }
 
 
-int empiler(pile_t * p, int data)
+int empiler(pile_t * p, lineTab_t data)
 {
     int boolean = 0;
     if (p->position < p->nbElts)
@@ -38,16 +42,15 @@ int empiler(pile_t * p, int data)
     return boolean;
 }
 
-pile_t* depiler(pile_t * p,int * code)
+pile_t* depiler(pile_t * p, lineTab_t * result)
 {
-    *code = -1;
     if (! (EstVidePile(p)))
     {
-        *code = p->tab[p->position];
+        *result = p->tab[p->position];
         p->position -= 1;
         
     } 
-    return p;
+    return p ;
 }
 
 void libererPile(pile_t * p)
@@ -65,10 +68,32 @@ void afficherPile(pile_t * p)
 {
     if (p != NULL)
     {
-        for (int i = p -> position ;i > -1 ;i--)
+        for (int i = p->position; i>-1; i--)
         {
-            printf("%d  ",p -> tab[i]);
+		for (int j=0; j<TAILLE; j++)
+            		printf("%d  ",p->tab[i].T[j]);
+		printf("\n") ;
         }
         printf("\n");
     } 
 }
+
+
+/*int main() {
+	
+	lineTab_t tab ;
+	tab.T[0] = 1 ;
+	tab.T[1] = 2 ;
+	tab.T[2] = 3 ;
+	tab.T[3] = 3 ;
+	pile_t * p = NULL;
+	p = init_pile(12) ;
+
+	for (int i=0; i<10 ; i++) {
+		empiler(p,  tab) ;
+	}
+	
+	afficherPile(p) ;
+
+	return EXIT_SUCCESS ;
+}*/
