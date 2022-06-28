aca79a253e0fc58afed4b358011b03a5f9b6cfc7
SoTraore <107843654+SoTraore@users.noreply.github.com>
2022-06-28 02:27:04 +0200

    Update pile.c
    Juste changer le type de retour de la fonction depiler

--- ProjetApprentissage/pile.c
+++ ProjetApprentissage/pile.c
@@ -1,4 +1,17 @@
-#include "pile.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define TAILLE 4
+
+typedef struct lineTab {
+	int T[TAILLE] ;
+} lineTab_t ;
+
+typedef struct pile
+{
+    int nbElts;
+    int position;
+    lineTab_t * tab ; 
+}pile_t;
 
 pile_t * init_pile(int taille)
 {
@@ -17,16 +30,14 @@ int EstVidePile(pile_t *p)
 }
 
 
-int empiler(pile_t * p, lineTab_t data)
+pile_t *  empiler(pile_t * p, lineTab_t data)
 {
-    int boolean = 0;
     if (p->position < p->nbElts)
     {
         p->position += 1;
-        p->tab[p->position]=data; 
-        boolean = 1;
+        p->tab[p->position] = data; 
     } 
-    return boolean;
+    return p ;
 }
 
 pile_t* depiler(pile_t * p, lineTab_t * result)
@@ -64,3 +75,23 @@ void afficherPile(pile_t * p)
         printf("\n");
     } 
 }
+
+/*
+int main() {
+	
+	lineTab_t tab ;
+	tab.T[0] = 1 ;
+	tab.T[1] = 2 ;
+	tab.T[2] = 3 ;
+	tab.T[3] = 3 ;
+	pile_t * p = NULL;
+	p = init_pile(12) ;
+
+	for (int i=0; i<10 ; i++) {
+		empiler(p,  tab) ;
+	}
+	
+	afficherPile(p) ;
+
+	return EXIT_SUCCESS ;
+}*/
